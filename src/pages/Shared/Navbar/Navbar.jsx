c7c015ac1b7d9cfa9160de328732bb2531f69b0c
talha <mdtalha2008@gmail.com>
2025-05-29 02:03:32 +0600

    banner and card add

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,95 +1,102 @@
-import React from 'react';
+import React from "react";
 import { FaCartPlus } from "react-icons/fa";
 import { GoPeople } from "react-icons/go";
-import logo from "../../../../src/assets/logo.png"
+import logo from "../../../../src/assets/logo.png";
 const Navbar = () => {
-    return (
-        <div>
-          <div className="w-full border-b"  style={{ fontFamily: "Parkinsans, serif", fontWeight: 400 }}>
-      {/* Top Alert Bar */}
-      <div className="flex items-center justify-between bg-white px-6 py-2 text-sm border-b">
-        <div className="flex items-center space-x-4">
-          <div className="bg-indigo-600 text-white font-semibold px-3 py-1 rounded">
-            COVID-19 ALERT!
+  return (
+    <div>
+      <div
+        className="w-full border-b"
+        style={{ fontFamily: "Parkinsans, serif", fontWeight: 400 }}
+      >
+        {/* Top Alert Bar */}
+        <div className="flex items-center justify-between bg-white px-6 py-2 text-sm border-b">
+          <div className="flex items-center space-x-4">
+            <div className="bg-indigo-600 text-white font-semibold px-3 py-1 rounded">
+              COVID-19 ALERT!
+            </div>
+            <span className="text-gray-700">
+              We are open with limited hours and staff.
+            </span>
           </div>
-          <span className="text-gray-700">
-            We are open with limited hours and staff.
-          </span>
+          <div className="flex items-center space-x-4 text-sm text-gray-700">
+            <div className="flex items-center space-x-1">
+              <span className="text-orange-500">📞</span>
+              <span>(+08) – 1850 – 1865</span>
+            </div>
+            <div className="flex items-center space-x-1">
+              <span className="text-orange-500">✉️</span>
+              <span>hello@modeltheme.com</span>
+            </div>
+          </div>
         </div>
-        <div className="flex items-center space-x-4 text-sm text-gray-700">
-          <div className="flex items-center space-x-1">
-            <span className="text-orange-500">📞</span>
-            <span>(+08) – 1850 – 1865</span>
+
+        {/* Main Navbar */}
+        <div className="flex items-center justify-between py-4">
+          {/* Logo */}
+          <div className="flex items-center space-x-2">
+            <div>
+              <img className="h-16 w-16" src={logo} alt="" />
+            </div>
+            <div className="flex flex-col">
+              <span className="text-2xl font-semibold">
+                Modern Homeo Pharmacy
+              </span>
+              <span className="text-left">Saidpur</span>
+            </div>
           </div>
-          <div className="flex items-center space-x-1">
-            <span className="text-orange-500">✉️</span>
-            <span>hello@modeltheme.com</span>
-          </div>
-        </div>
-      </div>
 
-      {/* Main Navbar */}
-      <div className="flex items-center justify-between py-4">
-        {/* Logo */}
-        <div className="flex items-center space-x-2">
-  <div>
-    <img className="h-16 w-16" src={logo} alt="" />
-  </div>
-  <div className="flex flex-col">
-    <span className="text-2xl font-semibold">Modern Homeo Pharmacy</span>
-    <span className="text-left">Saidpur</span>
-  </div>
-</div>
-
-
-        {/* Nav Links */}
-        <nav className="flex space-x-6 text-lg font-medium">
-          {/* <div className="relative group">
+          {/* Nav Links */}
+          <nav className="flex space-x-6 text-lg font-medium">
+            {/* <div className="relative group">
             <a href="#" className="text-black hover:text-red-500">
               Home
             </a>
             <div className="absolute left-0 bottom-[-2px] w-full h-[2px] bg-green-900"></div>
           </div> */}
-          <div className="relative group">
+            <div className="relative group">
+              <a href="#" className="text-green-800 hover:text-black">
+                Shop
+              </a>
+              <div className="absolute left-0 bottom-[-2px] w-full h-[2px] bg-green-900"></div>
+            </div>
             <a href="#" className="text-green-800 hover:text-black">
-              Shop
+              Hand Care
             </a>
-            <div className="absolute left-0 bottom-[-2px] w-full h-[2px] bg-green-900"></div>
-          </div>
-          <a href="#" className="text-green-800 hover:text-black">
-            Hand Care
-          </a>
-          <a href="#" className="text-green-800 hover:text-black">
-            Pain & Fever
-          </a>
-          <a href="#" className="text-green-800 hover:text-black">
-            Vitamins
-          </a>
-          <div className="relative group">
             <a href="#" className="text-green-800 hover:text-black">
-              Pages
+              Pain & Fever
             </a>
-          </div>
-          <a href="#" className="text-green-800 hover:text-black">
-            News
-          </a>
-          <div className="relative group">
             <a href="#" className="text-green-800 hover:text-black">
-              Blog
+              Vitamins
             </a>
-          </div>
-        </nav>
+            <div className="relative group">
+              <a href="#" className="text-green-800 hover:text-black">
+                Pages
+              </a>
+            </div>
+            <a href="#" className="text-green-800 hover:text-black">
+              News
+            </a>
+            <div className="relative group">
+              <a href="#" className="text-green-800 hover:text-black">
+                Blog
+              </a>
+            </div>
+          </nav>
 
-        {/* Icons */}
-        <div className="flex items-center space-x-6 text-2xl text-gray-700">
-           
-          <span className="cursor-pointer"><FaCartPlus className='text-black hover:text-green-700'/></span>
-          <span className="cursor-pointer"><GoPeople className='text-green-700 hover:text-black'/></span>
+          {/* Icons */}
+          <div className="flex items-center space-x-6 text-2xl text-gray-700">
+            <span className="cursor-pointer">
+              <FaCartPlus className="text-black hover:text-green-700" />
+            </span>
+            <span className="cursor-pointer">
+              <GoPeople className="text-green-700 hover:text-black" />
+            </span>
+          </div>
         </div>
       </div>
     </div>
-        </div>
-    );
+  );
 };
 
 export default Navbar;
