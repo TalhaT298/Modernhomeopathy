40105ab7fb45968601fde03d61d7768371be0d37
talha <mdtalha2008@gmail.com>
2025-05-30 19:46:51 +0600

    latest product add

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Card from '../Card/Card';
+import LatestProducts from '../LatestProducts/LatestProducts';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Card></Card>
+            <LatestProducts></LatestProducts>
         </div>
     );
 };
