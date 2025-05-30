e157e47a6a4e482700ccb36bf1bf51657bed0972
talha <mdtalha2008@gmail.com>
2025-05-31 00:55:17 +0600

    popular add at home.jsx

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 // import Card from '../Card/Card';
 import LatestProducts from '../LatestProducts/LatestProducts';
+import PopularCategories from '../PopularCategories/PopularCategories';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             {/* <Card></Card> */}
+            <PopularCategories></PopularCategories>
             <LatestProducts></LatestProducts>
         </div>
     );
