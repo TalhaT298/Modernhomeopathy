d66d367f0e98c109ddf66d3b7703659023e46859
talha <mdtalha2008@gmail.com>
2025-06-15 01:09:42 +0600

    special add at home.jsx

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from '../Banner/Banner';
 // import Card from '../Card/Card';
 import LatestProducts from '../LatestProducts/LatestProducts';
 import PopularCategories from '../PopularCategories/PopularCategories';
+import Specials from '../Specials/Specials';
 
 const Home = () => {
     return (
@@ -11,6 +12,7 @@ const Home = () => {
             {/* <Card></Card> */}
             <PopularCategories></PopularCategories>
             <LatestProducts></LatestProducts>
+            <Specials></Specials>
         </div>
     );
 };
